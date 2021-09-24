fc4150344d91e8ec539b887d672709e0ca75c549
Fitzroy <dev@zklink.org>
2021-09-24 22:20:13 +0800

    fix: accepter contract approve erc20

--- contracts/test/zkLink.sol
+++ contracts/test/zkLink.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.6.12;
 interface zkLink {
     function accept(address accepter, address receiver, uint16 tokenId, uint128 amount, uint16 withdrawFee, uint32 nonce) external payable;
     function getPendingBalance(address _address, address _token) external view returns (uint128);
+    function tokenAddresses(uint16 tokenId) external view returns(address);
 }
--- src/utils/prikey-tools.ts
+++ src/utils/prikey-tools.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import { providers, networkMap } from "../conf";
 import zkLink from "../zkLink.json"
 import BrokerAccepter from "../../build/BrokerAccepter.json"
+import MockErc20 from "../MockErc20.json"
 // import MockErc20 from '../MockErc20.json'
 import secret from "../../secret.json"
 import AccepterContractAddress from '../../accepter_contract_address.json'
@@ -13,10 +14,11 @@ import contract_addrss from "../../contract_address.json"
 import { Contract, Wallet, utils, BigNumber } from "ethers"
 import { formatEther, parseEther } from '@ethersproject/units';
 import { TransactionRequest } from '@ethersproject/providers';
+import { json } from 'body-parser';
 // -c : keys count
 // -p : keys path
 // -t : type : {create, list , approve, transfer}
-var argv = require('minimist')(process.argv.slice(2), { 'string': ['p', 't', 'amount', 'min_amount', 'contract'] });
+var argv = require('minimist')(process.argv.slice(2), { 'string': ['p', 't', 'amount', 'min_amount', 'contract','taddr'] });
 
 let count = argv['c'] || 10;
 let keysPath = argv["p"] || process.cwd();
@@ -35,6 +37,7 @@ async function main() {
         case "deploy": await deployBrokerAccepter(); break;
         case "batchapprove": await batchApprove(); break;
         case "batchtransfer": await batchTransfer(); break;
+        case "approvezklink":await approveZklink();break;
         default: {
             console.log("\nprivate key manage tool")
             console.log("-t : create|list|approve|transfer");
@@ -189,7 +192,7 @@ async function batchApprove() {
     console.log(spenders);
     let batch = new Contract(AccepterContractAddress[networkName], JSON.stringify(BrokerAccepter.abi), accepter);
     let gasLimit = spenders.length * 50000;
-    let tx = await batch.connect(accepter).batchApprove(contract_addrss[networkName], spenders, tokenId, BigNumber.from("0xffffffced"), { gasLimit: gasLimit })
+    let tx = await batch.connect(accepter).batchApprove(contract_addrss[networkName], spenders, tokenId, BigNumber.from("0xffffffffffffffffffffffffffffffff"), { gasLimit: gasLimit })
     console.log(tx);
 }
 
@@ -203,12 +206,28 @@ async function batchTransfer() {
     let gasLimit = spenders.length * 30000;
     let _amount = parseEther(amount);
     let value = _amount.mul(spenders.length + "")
-    let _fileter = parseEther(minAmount);
-    console.log(_amount);
-    console.log(value);
-    console.log(_fileter);
     let tx = await batch.connect(accepter).bacthTransfer(spenders, _amount, parseEther(minAmount), { gasLimit: gasLimit, value: value })
     console.log(tx);
+}
+// --cid --taddr 
+async function approveZklink() {
+    let [_, networkName] = getSpendersAndNetworkName();
+    let accepter = new Wallet(secret['accepter-key'], providers[networkName]);
+    let batch = new Contract(AccepterContractAddress[networkName], JSON.stringify(BrokerAccepter.abi), accepter);
+
+    let zkLinkContract = new Contract(contract_addrss[networkName], JSON.stringify(zkLink.abi), accepter);
+
+    let tokenAddress = argv['taddr'];//"0xe583769738b6dd4E7CAF8451050d1948BE717679";//await zkLinkContract.connect(accepter).tokenAddresses(tokenId);
+    console.log("tokenAddress: ", tokenAddress);
+    let erc20 = new Contract(tokenAddress, JSON.stringify(MockErc20.abi), accepter);
+    let data = erc20.interface.encodeFunctionData("approve(address,uint256)", [contract_addrss[networkName], BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")]);
+
+    let gasLimit = 100000;
+    let tx = await batch.connect(accepter).dynamicCall(tokenAddress, data, { gasLimit: gasLimit });
+    console.log(tx);
+
+    let _allowance_data = await erc20.connect(accepter).allowance(AccepterContractAddress[networkName],contract_addrss[networkName])
+    console.log(_allowance_data)
 }
 
 //--cid
